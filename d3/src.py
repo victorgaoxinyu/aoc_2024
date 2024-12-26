21a3fc3860910a6fc5787b09b4f5d31613596beb
victor Gao <victorgao@victors-MBP-2.modem>
2024-12-26 19:52:04 +1100

    d3 q2

--- d3/src.py
+++ d3/src.py
@@ -5,16 +5,28 @@ import re
 idir = "input"
 
 pattern = r"mul\((\d{1,3}),(\d{1,3})\)"
+flag_pattern = r"do\(\)|don't\(\)"
+
+combined_pattern = rf"{pattern}|{flag_pattern}"
+
 sum = 0
+enabled = True
 
 with open(idir, "r") as f:
 	line = f.readline()
 
 	while line:
-		matches = re.findall(pattern, line)
-
-		for x, y in matches:
-			sum += int(x) * int(y)
+		# print(line)
+		for match in re.finditer(combined_pattern, line):
+			if match.group(1) and match.group(2):
+				if enabled:
+					x = int(match.group(1))
+					y = int(match.group(2))
+					sum += x * y
+			elif match.group(0) == "do()":
+				enabled = True
+			elif match.group(0) == "don't()":
+				enabled = False
 
 		line = f.readline()
 
