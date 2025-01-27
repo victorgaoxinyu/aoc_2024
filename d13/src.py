637d52e5a3072a9038cdf6c45db4128cf9366ea8
victor Gao <victorgaonewlife@gmail.com>
2025-01-27 12:01:44 +1100

    d13 q1, ez but not opt

---
+++ d13/src.py
@@ -0,0 +1,64 @@
+import re
+idir = 'input'
+
+def find_combination(target_x, target_y, op_a, op_b):
+	solutions = []
+	x_a, y_a = op_a
+	x_b, y_b = op_b
+	max_a = target_x // x_a
+	for count_a in range(max_a + 1):
+		remaining_x = target_x - count_a * x_a
+		remaining_y = target_y - count_a * y_a
+
+		if remaining_x % x_b == 0 and remaining_y % y_b == 0:
+			count_b = remaining_x // x_b
+			if count_b >= 0 and count_b * y_b == remaining_y:
+				solutions.append((count_a, count_b))
+
+	return solutions
+
+
+def get_cheapest_comb(solutions):
+	lowest = float('inf')
+	for ca, cb in solutions:
+		lowest = min(lowest, ca * 3 + cb * 1)
+
+	return lowest if lowest != float('inf') else 0
+
+
+# [((Ax, Ay), (Bx, By), (Px, Py))]
+
+with open(idir, 'r') as f:
+	idx = 1
+	total = 0
+	line = f.readline()
+
+	while line:	
+		if line == "\n":
+			line = f.readline()
+			continue
+
+		matches = re.findall(r'\d+', line)
+		numbers = list(map(int, matches))
+		if idx % 3 == 1:
+			# A
+			op_a = numbers
+		elif idx % 3 == 2:
+			# B
+			op_b = numbers
+		else:
+			target_x, target_y = numbers
+			# print(op_a, op_b, target_x, target_y)
+
+			comb = find_combination(target_x, target_y, op_a, op_b)
+			cheapest = get_cheapest_comb(comb)
+			total += cheapest
+
+			# print(cheapest)
+
+		# input()
+		idx += 1
+		line = f.readline()
+
+print(total)
+
