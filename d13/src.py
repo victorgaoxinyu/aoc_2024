581f76b399e52afac42a68c822c69dc299bda928
victor Gao <victorgaonewlife@gmail.com>
2025-01-27 13:08:49 +1100

    d13 q2, Cramer's rule

--- d13/src.py
+++ d13/src.py
@@ -1,5 +1,7 @@
 import re
+from fractions import Fraction
 idir = 'input'
+
 
 def find_combination(target_x, target_y, op_a, op_b):
 	solutions = []
@@ -7,6 +9,7 @@ def find_combination(target_x, target_y, op_a, op_b):
 	x_b, y_b = op_b
 	max_a = target_x // x_a
 	for count_a in range(max_a + 1):
+		print(count_a)
 		remaining_x = target_x - count_a * x_a
 		remaining_y = target_y - count_a * y_a
 
@@ -14,6 +17,7 @@ def find_combination(target_x, target_y, op_a, op_b):
 			count_b = remaining_x // x_b
 			if count_b >= 0 and count_b * y_b == remaining_y:
 				solutions.append((count_a, count_b))
+
 
 	return solutions
 
@@ -24,6 +28,24 @@ def get_cheapest_comb(solutions):
 		lowest = min(lowest, ca * 3 + cb * 1)
 
 	return lowest if lowest != float('inf') else 0
+
+
+def find_combination_q2(target_x, target_y, op_a, op_b):
+	target_x += 10000000000000
+	target_y += 10000000000000
+	solutions = []
+	x_a, y_a = op_a # a c
+	x_b, y_b = op_b # b d
+
+	# this is Cramer's rule
+	if x_a * y_b - y_a * x_b != 0:
+		i = Fraction(y_b, x_a * y_b - x_b * y_a) * target_x + Fraction(-x_b, x_a * y_b - x_b * y_a) * target_y
+		j = Fraction(-y_a, x_a * y_b - x_b * y_a) * target_x + Fraction(x_a, x_a * y_b - x_b * y_a) * target_y
+		if i > 0 and j > 0 and i == int(i) and j == int(j):
+			assert x_a * i + x_b * j == target_x
+			assert y_a * i + y_b * j == target_y
+			solutions.append((i, j))
+	return solutions
 
 
 # [((Ax, Ay), (Bx, By), (Px, Py))]
@@ -48,13 +70,13 @@ with open(idir, 'r') as f:
 			op_b = numbers
 		else:
 			target_x, target_y = numbers
-			# print(op_a, op_b, target_x, target_y)
+			# print("*" * 10)
 
-			comb = find_combination(target_x, target_y, op_a, op_b)
+			# comb = find_combination(target_x, target_y, op_a, op_b)
+			comb = find_combination_q2(target_x, target_y, op_a, op_b)			
+			# print(comb)
 			cheapest = get_cheapest_comb(comb)
 			total += cheapest
-
-			# print(cheapest)
 
 		# input()
 		idx += 1
